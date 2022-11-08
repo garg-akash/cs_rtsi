d70f16c95ea23f2c6d8fc9bf5725b5c08abced33
garg-akash <akashgrg2@gmail.com>
2022-11-07 18:00:45 -0800

    single recipe [not working in new sim]

--- examples/setData_test.cpp
+++ examples/setData_test.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 #include <thread>
 
-const std::string hostip = "192.168.51.139";
+const std::string hostip = "192.168.133.129";
 
 int main(int argc, char const *argv[])
 {
   bool verbose = true;
   RTSIIOInterface rtsi_io(hostip, verbose);
 
-  rtsi_io.setStandardDigitalOut(11, true);
+  // rtsi_io.setStandardDigitalOut(11, true);
   rtsi_io.setConfigurableDigitalOut(6, true);
 
   std::cout << "IO set\n";

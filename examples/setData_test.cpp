2efeecfd4d5a0622ff28641d7d5fb8e786f3a6c4
garg-akash <akashgrg2@gmail.com>
2022-11-15 09:52:59 +0800

    input subscription read from json file

--- examples/setData_test.cpp
+++ examples/setData_test.cpp
@@ -1,36 +1,73 @@
 /*
 Author: Elite_akashgarg
 CreateDate: 2022-11-09
+LastEdited: 2022-11-15
 Description: example script to test input subscription
 */
 #include <cs_rtsi/rtsi_io_interface.h>
 #include <iostream>
 #include <thread>
+#include <jsoncpp/json/json.h>
+#include <fstream>
 
 const std::string hostip = "192.168.133.129";
+const std::string recipe_file = "../examples/recipe.json";
 
 int main(int argc, char const *argv[])
 {
   bool verbose = true;
   RTSIIOInterface rtsi_io(hostip, verbose);
+  std::ifstream recipes(recipe_file, std::ifstream::binary);
+  Json::Value i_recipes;
+  recipes >> i_recipes;
+  std::vector<std::string> variables;
+  for(auto i : i_recipes["input"])
+  {
+    // std::cout << i["subs"] << " " << i["number"] << " " << i["value"] << "\n";
+    if(i["subs"].asString() == "speed_slider_fraction")
+      rtsi_io.setSpeedSlider(i["value"].asDouble());
 
-  // rtsi_io.setSpeedSlider(1);
-  // rtsi_io.setInputDoubleRegister(18,0.3);
-  rtsi_io.setStandardDigitalOut(15, true);
-  rtsi_io.setConfigurableDigitalOut(0, true);
-  rtsi_io.setAnalogOutputVoltage(0,0.3);
-  rtsi_io.setAnalogOutputCurrent(1,0.7);
-  rtsi_io.setInputIntRegister(47,19);
-  rtsi_io.setInputDoubleRegister(47,0.19);
-  rtsi_io.setInputBitRegister0to31(1);
-  rtsi_io.setInputBitRegister32to63(3);
-  rtsi_io.setInputBitRegister(64,false);
-  // for(int i = 0; i  < 50; i++)
-  // {
-  //   rtsi_io.setInputDoubleRegisterPosition(0.671, -0.460 + 0.001*i, 0.542);
-  //   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  // }
-  // rtsi_io.setInputDoubleRegisterPosition(0, 0, 0);
+    else if(i["subs"].asString() == "standard_digital_output")
+      rtsi_io.setStandardDigitalOut(i["number"].asUInt(), i["value"].asBool());
+
+    else if(i["subs"].asString() == "configurable_digital_output")
+      rtsi_io.setConfigurableDigitalOut(i["number"].asUInt(), i["value"].asBool());
+
+    else if(i["subs"].asString() == "standard_analog_output_0")
+    {
+      if(i["type"].asBool() == 0)
+        rtsi_io.setAnalogOutputCurrent(0, i["value"].asDouble());
+      if(i["type"].asBool() == 1)
+        rtsi_io.setAnalogOutputVoltage(0, i["value"].asDouble());
+    }
+
+    else if(i["subs"].asString() == "standard_analog_output_1")
+    {
+      if(i["type"].asBool() == 0)
+        rtsi_io.setAnalogOutputCurrent(1, i["value"].asDouble());
+      if(i["type"].asBool() == 1)
+        rtsi_io.setAnalogOutputVoltage(1, i["value"].asDouble());
+    }
+
+    else if(i["subs"].asString() == "input_bit_registers0_to_31")
+      rtsi_io.setInputBitRegister0to31(i["value"].asInt());
+
+    else if(i["subs"].asString() == "input_bit_registers32_to_64")
+      rtsi_io.setInputBitRegister32to63(i["value"].asInt());
+
+    else if(i["subs"].asString() == "input_bit_registerX")
+      rtsi_io.setInputBitRegister(i["number"].asInt(), i["value"].asBool());
+
+    else if(i["subs"].asString() == "input_int_registerX")
+      rtsi_io.setInputIntRegister(i["number"].asInt(), i["value"].asInt());
+
+    else if(i["subs"].asString() == "input_double_registerX")
+      rtsi_io.setInputDoubleRegister(i["number"].asInt(), i["value"].asDouble());
+
+    else
+      throw std::runtime_error("Invalid input subscription requested : " + i["subs"].asString());
+  }
+
   std::cout << "IO set\n";
 
   return 0;
